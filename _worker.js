a1a7fa8f1c27f958c9b3e08c803f0860aa4829ce
proservermanager <168148396+proservermanager@users.noreply.github.com>
2024-05-06 17:59:22 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '0d0af185-525c-4ae6-8af8-2dc52cb08bb0';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
